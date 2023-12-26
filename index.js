7a99e407fefc4d51b31ace15dae0834070f3465c
bhumitjaviya54 <93394436+bhumitjaviya54@users.noreply.github.com>
2023-12-26 16:31:07 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// Number Data Type
+
+// Number represnts integars and floating point numbers.
+
+let num = 100; //int number
+let num1 = 98.95; //float number
+
+console.log(num);
+console.log(num1);
